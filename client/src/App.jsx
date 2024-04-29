341a6f9557dc161ccb239a6b71bf5e36a41fdc38
Saumitra <saumitraupadhyay28@gmail.com>
2024-04-29 14:50:21 +0530

    Added Navbar component to Layout in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Dashboard from "./pages/Dashboard";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Sidebar from "./components/Sidebar";
-
+import Navbar from "./components/Navbar";
 function Layout() {
   const {user }= useSelector((state) => state.auth);
   const location = useLocation();
@@ -22,7 +22,7 @@ function Layout() {
       </div>
       {/* <MobileSidebar/> */}
       <div className="flex-1 overflow-y-auto">
-        {/* <Navbar/> */}
+        <Navbar/>
         <div className="p-4 2xl:px-10">
           <Outlet />
         </div>
