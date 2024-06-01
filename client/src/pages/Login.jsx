75ba1b6e3c28163982aadcfd297dd0ab77123eaf
Saumitra <saumitraupadhyay28@gmail.com>
2024-06-01 18:45:20 +0530

    integrated login and Avatar components to backend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,6 +4,11 @@ import { useNavigate } from "react-router-dom";
 import Textbox from "../components/Textbox";
 import Button from "../components/Button";
 import { useSelector } from "react-redux";
+import { toast } from "sonner";
+import { useLoginMutation } from "../redux/slices/api/authApiSlice";
+import { useDispatch } from "react-redux";
+import { setCredentials } from "../redux/slices/authSlice";
+// import{ Loading} from "../components/Loader";
 
 const Login = () => {
   const { user } = useSelector((state) => state.auth);
@@ -15,18 +20,31 @@ const Login = () => {
   } = useForm();
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [login, isLoading] = useLoginMutation();
 
   const submitHandler = async (data) => {
-    console.log("submit");
+    try {
+      const result = await login(data);
+      //if pw or email is wrong
+      if (result.error) { 
+        toast.error("Invalid email or password!");
+        return;
+      }
+      dispatch(setCredentials(result));
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.data?.message || error.message);
+    }
   };
-  console.log("User data: ",user);
   
   useEffect(() => {
     user && navigate("/dashboard");
   }, [user]);
 
   return (
-    <div className='w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]'>
+    <div className='w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#100C08]'>
       <div className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
         {/* left side */}
         <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
@@ -35,8 +53,8 @@ const Login = () => {
               Manage all your task in one place!
             </span>
             <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700'>
-              <span>Cloud-Based</span>
-              <span>Task Manager</span>
+              <span>Simplify Your</span> 
+              <span>Workflow</span>
             </p>
 
             <div className='cell'>
@@ -49,13 +67,13 @@ const Login = () => {
         <div className='w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center'>
           <form
             onSubmit={handleSubmit(submitHandler)}
-            className='form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14'
+            className='form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-[#030637] px-10 pt-14 pb-14'
           >
             <div className=''>
               <p className='text-blue-600 text-3xl font-bold text-center'>
                 Welcome back!
               </p>
-              <p className='text-center text-base text-gray-700 '>
+              <p className='text-center text-base text-gray-400 '>
                 Keep all your credential safge.
               </p>
             </div>
@@ -91,7 +109,7 @@ const Login = () => {
               <Button
                 type='submit'
                 label='Submit'
-                className='w-full h-10 bg-blue-700 text-white rounded-full'
+                className='w-full h-10 bg-[#2B2A4C] text-white rounded-full hover:black hover:bg-opacity-60'
               />
             </div>
           </form>
--- client/src/redux/slices/authSlice.js
+++ client/src/redux/slices/authSlice.js
@@ -1,30 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {user as initialUser} from "../../assets/data";
+import { user } from "../../assets/data";
 
 const initialState = {
-  user: initialUser,
+  user: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
+
   isSidebarOpen: false,
 };
-
+console.log(initialState);
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setCredentials(state, action) {
-      state.user = action.payload;
-      // Optionally update localStorage if needed
-      // localStorage.setItem("userInfo", JSON.stringify(action.payload));
+    setCredentials: (state, action) => {
+      state.user = action.payload.data;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload.data));
     },
-    logout(state) {
+    logout: (state, action) => {
       state.user = null;
-      // Optionally remove from localStorage if needed
-      // localStorage.removeItem("userInfo");
+      localStorage.removeItem("userInfo");
     },
-    setOpenSidebar(state, action) {
+    setOpenSidebar: (state, action) => {
       state.isSidebarOpen = action.payload;
     },
   },
 });
 
 export const { setCredentials, logout, setOpenSidebar } = authSlice.actions;
+
 export default authSlice.reducer;
