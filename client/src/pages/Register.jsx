6cbbb1d6d85650b2b954522f14c219a7c928a2f0
Saumitra <saumitraupadhyay28@gmail.com>
2024-06-04 23:41:29 +0530

    fixing some issues

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -44,10 +44,10 @@ const Register = () => {
         <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
           <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
             <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700'>
-              <span>Simplify Your</span>
-              <span>Workflow</span>
+              <span>Organize</span>
+              <span>with Precision</span>
             </p>
-            <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base border-gray-300 text-gray-600'>
+            <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base border-gray-300 text-gray-400'>
               Your team without TaskShpere 😉
             </span>
             <div className='cell'>
